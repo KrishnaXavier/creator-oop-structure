99f310cd6af3628d22e3a0f92d1a785f15b70908
KrishnaXavier <tsixav@gmail.com>
2017-08-22 09:28:34 -0300

    versão 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const entrada = document.getElementById("entrada")
-const saida	  = document.getElementById("saida")
-const criar	  = document.getElementById("criar")
-const constru = document.getElementById("construturo")
+const entrada 	= document.getElementById("entrada")
+const saida	  	= document.getElementById("saida")
+const criar	  	= document.getElementById("criar")
+const classe	= document.getElementById("classe")
 
 window.addEventListener('DOMContentLoaded', init)
 
@@ -11,21 +11,21 @@ function init(){
 }
 
 function gerar(){	
-	saida.innerHTML = gerarMet()
+	saida.innerHTML = "class " + classe.value +"{\n" + gerarEscopo() + "\n" + gerarCon() + "\n" + gerarMet() + "\n}";
 	autoGrow(saida)
-	constru.innerHTML = gerarCon()	
-	autoGrow(constru)
+	// constru.innerHTML = gerarCon()	
+	// autoGrow(constru)
 }
 
 function gerarMet(){
 	console.log("gerarMet");
 	let a = entrada.value.split(", ");
 	let strSaida = "";
-	let visib = document.querySelector('input[name="visibilidade"]:checked').value
+	let visib = document.querySelector('input[name="visib-metodos"]:checked').value
 	for(i=0; i<a.length; i++){
 		newA = a[i].charAt(0).toUpperCase() + a[i].substr(1);						
-		strSaida += visib+" function get"+newA+"(){ \n return $this->"+a[i]+"; \n} \n";
-		strSaida += visib+" function set"+newA+"($"+a[i]+"){ \n  $this->"+a[i]+" = $"+a[i]+"; \n} \n";		
+		strSaida += visib+" function get"+newA+"(){ \n	return $this->"+a[i]+"; \n} \n";
+		strSaida += visib+" function set"+newA+"($"+a[i]+"){ \n	$this->"+a[i]+" = $"+a[i]+"; \n} \n";		
 	}	
 	return strSaida;	
 }
@@ -40,13 +40,25 @@ function gerarCon(){
 		else				
 			strConHead += "$"+a[i]+", ";
 
-		strBody += "$this->"+a[i]+" = $"+a[i]+";\n";		
+		strBody += "	$this->"+a[i]+" = $"+a[i]+";\n";		
 	}
 
 	return "public function __construct(" + strConHead +"){\n"+ strBody + "}";	
 }
 
+function gerarEscopo(){
+	console.log("gerarMet");
+	let a = entrada.value.split(", ");
+	let strSaida = "";
+	let visib = document.querySelector('input[name="visib-atributos"]:checked').value
+	for(i=0; i<a.length; i++){				
+		strSaida += visib+" "+a[i]+"; \n";		
+	}	
+	return strSaida;
+
+}
+
 function autoGrow(element) {
-    element.style.height = "5px";
-    element.style.height = (element.scrollHeight)+"px";
+	element.style.height = "5px";
+	element.style.height = (element.scrollHeight)+"px";
 }
