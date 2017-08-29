d78a7950db2b19aecba7fbd3ee3695ec3def6a12
KrishnaXavier <tsixav@gmail.com>
2017-08-29 11:26:01 -0300

    verificação de entrada vazia e identação

--- js/script.js
+++ js/script.js
@@ -10,21 +10,42 @@ function init(){
 	gerar()
 }
 
-function gerar(){	
-	saida.innerHTML = "class " + classe.value +"{\n" + gerarEscopo() + "\n" + gerarCon() + "\n" + gerarMet() + "\n}";
+function gerar(){
+	if(classe.value != ""){		
+		strSaida = 
+		"class " + 
+		classe.value +
+		"{\n";
+
+		if(entrada.value != ""){
+			strSaida +=
+			gerarEscopo() + 
+			"\n" + 
+			gerarCon() + 
+			"\n" + 
+			gerarMet()
+		}
+
+		strSaida += "\n}";
+
+		saida.innerHTML = strSaida;
+	}
+	else{
+		saida.innerHTML = "\\\\classe vazia";
+	}
+	
 	autoGrow(saida)	
 }
 
-function gerarMet(){
-	console.log("gerarMet");
-	let a = entrada.value.split(", ");
+function gerarMet(){	
 	let strSaida = "";
+	let a = entrada.value.split(", ");	
 	let visib = document.querySelector('input[name="visib-metodos"]:checked').value
 	for(i=0; i<a.length; i++){
-		newA = a[i].charAt(0).toUpperCase() + a[i].substr(1);						
-		strSaida += visib+" function get"+newA+"(){ \n	return $this->"+a[i]+"; \n} \n";
-		strSaida += visib+" function set"+newA+"($"+a[i]+"){ \n	$this->"+a[i]+" = $"+a[i]+"; \n} \n";		
-	}	
+		newA = a[i].charAt(0).toUpperCase() + a[i].substr(1);
+		strSaida += "	"+visib+" function get"+newA+"(){ \n		return $this->"+a[i]+"; \n	} \n";
+		strSaida += "	"+visib+" function set"+newA+"($"+a[i]+"){ \n		$this->"+a[i]+" = $"+a[i]+"; \n	} \n";
+	}
 	return strSaida;	
 }
 
@@ -38,22 +59,20 @@ function gerarCon(){
 		else				
 			strConHead += "$"+a[i]+", ";
 
-		strBody += "	$this->"+a[i]+" = $"+a[i]+";\n";		
+		strBody += "		$this->"+a[i]+" = $"+a[i]+";\n";		
 	}
 
-	return "public function __construct(" + strConHead +"){\n"+ strBody + "}";	
+	return "	public function __construct(" + strConHead +"){\n"+ strBody + "	}";	
 }
 
-function gerarEscopo(){
-	console.log("gerarMet");
+function gerarEscopo(){	
 	let a = entrada.value.split(", ");
 	let strSaida = "";
 	let visib = document.querySelector('input[name="visib-atributos"]:checked').value
 	for(i=0; i<a.length; i++){				
-		strSaida += visib+" $"+a[i]+"; \n";		
+		strSaida += "	"+visib+" $"+a[i]+"; \n";		
 	}	
 	return strSaida;
-
 }
 
 function autoGrow(element) {
